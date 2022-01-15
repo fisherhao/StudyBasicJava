30dbe96cb30fd4e6b345ce277bb8062927d0df5a
Fisherhao <yh1024@aliyun.com>
2022-01-15 19:02:32 +0800

    提交1
    Signed-off-by: Fisherhao <yh1024@aliyun.com>

--- src/test/java/com/hao/yu/bean/FactoryBeanTest.java
+++ src/test/java/com/hao/yu/bean/FactoryBeanTest.java
@@ -24,14 +24,14 @@ public class FactoryBeanTest {
 
 	@org.junit.Test
 	public void  testBean1(){
-		System.out.println("FACTORYDE 任务1");
+		System.out.println("FactoryBean 任务1");
 		System.out.println(task1.getTaskDesc());
 		System.out.println(task1);
 	}
 
 	@org.junit.Test
-	public void  testBean2(){
-		System.out.println("FACTORYDE 任务2");
+	public void testBean2() {
+		System.out.println("FactoryBean 任务2");
 		System.out.println(task2.getTaskDesc());
 		System.out.println(task2);
 	}
