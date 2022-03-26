de9d03f71cdff0c443a60ce201c35586853ac37a
Fisher.hao <fine31@163.com>
2022-03-26 19:36:43 +0800

    提交test121
    Signed-off-by: Fisher.hao <fine31@163.com>

--- src/test/java/com/hao/yu/bean/FactoryBeanTest.java
+++ src/test/java/com/hao/yu/bean/FactoryBeanTest.java
@@ -33,7 +33,7 @@ public class FactoryBeanTest {
 	@org.junit.Test
 	public void testBean2() {
 
-		System.out.println("FactoryBean 任务2");
+		System.out.println("FactoryBean 任务 2");
 		System.out.println(task2.getTaskDesc());
 		System.out.println(task2.getTaskName());
 		System.out.println(task2);
