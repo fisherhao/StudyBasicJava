26db308630051564d479809323be197205ba502d
Fisher.hao <fine31@163.com>
2022-05-11 23:46:39 +0800

    test
    Signed-off-by: Fisher.hao <fine31@163.com>

---
+++ src/main/java/com/hao/yu/test/java8/StringTest.java
@@ -0,0 +1,16 @@
+package com.hao.yu.test.java8;
+
+import java.util.StringJoiner;
+
+/**
+ * 说明：
+ *
+ * @author： Fisher.Hao
+ * @date： 2022-05-11 23:40:45
+ */
+public class StringTest {
+
+	public static void main(String[] args) {
+		StringJoiner stringJoiner = new StringJoiner("&", "?", "");
+	}
+}
