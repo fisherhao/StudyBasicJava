0dec38fb9689cc886f98214c9ae43b33fa9a25b3
fisherhao <fine31@163.com>
2025-12-02 18:57:49 +0800

    提交

--- src/main/java/com/hao/yu/lc/T006_Z字型转换.java
+++ src/main/java/com/hao/yu/lc/T006_Z字型转换.java
@@ -1,5 +1,8 @@
 package com.hao.yu.lc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 说明：
  *
@@ -7,4 +10,100 @@ package com.hao.yu.lc;
  * @version v 0.1 2025年12月02日 星期二 11:36
  */
 public class T006_Z字型转换 {
+
+    public static void main(String[] args) {
+        String paypalishiring = convert("PAYPALISHIRING", 3);
+        System.out.println(paypalishiring);
+    }
+
+    public static String convert(String s, int numRows) {
+
+        if (numRows <= 1) {
+            return s;
+        }
+
+        //123432 123432 123432
+        char[] arr = s.toCharArray();
+
+        int level = 1;
+        List<ModelTest> list = new ArrayList<>();
+
+        boolean needPlus = true;
+        for (char c : arr) {
+
+            ModelTest test = new ModelTest();
+            test.setCh(c);
+            test.setLevel(level);
+
+            list.add(test);
+            if (needPlus) {
+                level++;
+            } else {
+                level--;
+            }
+            if (level == 1) {
+                needPlus = true;
+            }
+
+            if (level == numRows) {
+                needPlus = false;
+            }
+        }
+        StringBuilder builder = new StringBuilder();
+        for (int j = 1; j <= numRows; j++) {
+
+            for (ModelTest test : list) {
+                if (test.getLevel() == j) {
+                    builder.append(test.getCh());
+                }
+            }
+
+        }
+
+        return builder.toString();
+    }
+
+    static class ModelTest {
+        private Integer level;
+
+        private char ch;
+
+        /**
+         * Getter method for property <tt>level</tt>.
+         *
+         * @return property value of level
+         */
+        public Integer getLevel() {
+            return level;
+        }
+
+        /**
+         * Setter method for property <tt>level</tt>.
+         *
+         * @param level
+         *         value to be assigned to property
+         */
+        public void setLevel(Integer level) {
+            this.level = level;
+        }
+
+        /**
+         * Getter method for property <tt>ch</tt>.
+         *
+         * @return property value of ch
+         */
+        public char getCh() {
+            return ch;
+        }
+
+        /**
+         * Setter method for property <tt>ch</tt>.
+         *
+         * @param ch
+         *         value to be assigned to property
+         */
+        public void setCh(char ch) {
+            this.ch = ch;
+        }
+    }
 }
