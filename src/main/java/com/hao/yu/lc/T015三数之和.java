d5ea8bc7a3720b5d2a012b25901332e0c491ef36
fisherhao <fine31@163.com>
2025-12-05 00:20:09 +0800

    我的修改

--- src/main/java/com/hao/yu/lc/T015三数之和.java
+++ src/main/java/com/hao/yu/lc/T015三数之和.java
@@ -2,7 +2,6 @@ package com.hao.yu.lc;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -12,45 +11,87 @@ import java.util.List;
  * @version v 0.1 2025年12月04日 星期四 17:05
  */
 public class T015三数之和 {
-    public static List<List<Integer>> threeSum(int[] nums) {
+    public static List<List<Integer>> threeSum2(int[] nums) {
 
         Arrays.sort(nums);
+//        System.out.println(JSON.toJSONString(nums));
 
         List<List<Integer>> result = new ArrayList<>();
 
-        int i = 0;
+        //取出中间值
+        for (int j = 1; j < nums.length - 1; j++) {
+            //使用左右值
+            int begin = 0;
+            int end = nums.length - 1;
+            while (begin < j && j < end) {
+                while (begin + 1 < j && nums[begin + 1] == nums[begin]) {
+                    begin++;
+                }
 
-        int k = nums.length - 1;
-        while (i < k) {
+                while (end - 1 > j && nums[end] == nums[end - 1]) {
+                    end--;
+                }
+                if (nums[begin] + nums[j] + nums[end] == 0) {
+                    result.add(Arrays.asList(nums[begin], nums[j], nums[end]));
+                    begin++;
+                    end--;
+                } else if (nums[begin] + nums[j] + nums[end] < 0) {
+                    begin++;
+                } else {
+                    end--;
+                }
+            }
 
         }
 
         return result;
     }
 
-    public static List<List<Integer>> threeSum2(int[] nums) {
-        Integer[] numsTem = new Integer[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            numsTem[i] = nums[i];
-        }
-        List<Integer> list = Arrays.asList(numsTem);
-        list.sort(Comparator.comparingInt(Integer::intValue).reversed());
-
-        int length = nums.length;
-
+    public static List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        List<String> strList = new ArrayList<>();
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < i; j++) {
-                for (int k = 0; k < j; k++) {
-                    if (list.get(i) + list.get(j) + list.get(k) == 0) {
-                        String key = list.get(i) + "-" + list.get(j) + "-" + list.get(k);
-                        if (strList.contains(key)) {
-                            continue;
-                        }
-                        result.add(Arrays.asList(list.get(i), list.get(j), list.get(k)));
-                        strList.add(key);
+        if (nums == null || nums.length < 3) {
+            return result;
+        }
+
+        // 排序
+        Arrays.sort(nums);
+
+        // 外层循环：固定第一个数（i），范围是0到nums.length-3（保证后面有两个数）
+        for (int i = 0; i < nums.length - 2; i++) {
+            // 去重1：第一个数重复时，直接跳过（避免生成重复三元组）
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            // 双指针：left = i+1（第二个数），right = 末尾（第三个数）
+            int begin = i + 1;
+            int end = nums.length - 1;
+
+            while (begin < end) {
+                int sum = nums[i] + nums[begin] + nums[end];
+
+                if (sum == 0) {
+                    // 找到符合条件的三元组，加入结果
+                    result.add(Arrays.asList(nums[i], nums[begin], nums[end]));
+
+                    // 去重2：跳过begin的重复元素（避免重复的第二个数）
+                    while (begin < end && nums[begin] == nums[begin + 1]) {
+                        begin++;
                     }
+                    // 去重3：跳过end的重复元素（避免重复的第三个数）
+                    while (begin < end && nums[end] == nums[end - 1]) {
+                        end--;
+                    }
+
+                    // 双指针同时移动（找下一组可能的数）
+                    begin++;
+                    end--;
+                } else if (sum < 0) {
+                    //左指针右移（增大数值）
+                    begin++;
+                } else {
+                    // 右指针左移（减小数值）
+                    end--;
                 }
             }
         }
@@ -60,10 +101,12 @@ public class T015三数之和 {
 
     public static void main(String[] args) {
 
-        int[] nums = {-1, 0, 1, 2, -1, -4};
-        List<List<Integer>> lists = threeSum2(nums);
-        System.out.println(lists);
+        int[] nums = new int[]{
+                0, 0, 0, -1, 0, 1, 2, -1, -4, 9, 9, 9, 9, 9
+        };
 
-        System.out.printf("11{0}", threeSum(nums));
+        List<List<Integer>> threeSum = threeSum(nums);
+        System.out.println(threeSum);
+
     }
 }
