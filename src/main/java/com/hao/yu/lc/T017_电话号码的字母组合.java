19c1a6e44b9864d8e57b2745292875faa143d331
fisherhao <fine31@163.com>
2025-12-05 17:58:51 +0800

    提交

--- src/main/java/com/hao/yu/lc/T017_电话号码的字母组合.java
+++ src/main/java/com/hao/yu/lc/T017_电话号码的字母组合.java
@@ -1,6 +1,11 @@
 package com.hao.yu.lc;
 
+import com.alibaba.fastjson.JSON;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 说明：
@@ -10,11 +15,52 @@ import java.util.List;
  */
 public class T017_电话号码的字母组合 {
     public static void main(String[] args) {
-
+        List<String> strings = letterCombinations("239");
+        System.out.println(JSON.toJSONString(strings));
     }
 
-    public List<String> letterCombinations(String digits) {
+    public static List<String> letterCombinations(String digits) {
 
-        return null;
+        Map<Integer, String[]> map = new HashMap<>();
+        map.put(2, new String[]{"a", "b", "c"});
+        map.put(3, new String[]{"d", "e", "f"});
+        map.put(4, new String[]{"g", "h", "i"});
+        map.put(5, new String[]{"j", "k", "l"});
+        map.put(6, new String[]{"m", "n", "o"});
+        map.put(7, new String[]{"p", "q", "r", "s"});
+        map.put(8, new String[]{"t", "u", "v"});
+        map.put(9, new String[]{"w", "x", "y", "z"});
+
+        char[] charArray = digits.toCharArray();
+
+        List<String> result = new ArrayList<>();
+
+        boolean notFirst = true;
+        for (int i = 0; i < charArray.length; i++) {
+
+            char c = charArray[i];
+            Integer num = c - '0';
+            String[] str = map.get(num);
+
+            if (result.isEmpty()) {
+                for (String s1 : str) {
+                    result.add(s1);
+                }
+            }
+
+            List<String> temp = new ArrayList<>();
+            if (!notFirst) {
+                for (String s1 : str) {
+                    for (String s : result) {
+                        temp.add(s + s1);
+                    }
+                }
+            }
+            if (!temp.isEmpty()) {
+                result = temp;
+            }
+            notFirst = false;
+        }
+        return result;
     }
 }
