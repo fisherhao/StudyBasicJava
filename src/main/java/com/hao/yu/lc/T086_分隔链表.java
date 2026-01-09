105fda4d2510fda9af9a928a04d426bc865f47ad
fisherhao <fine31@163.com>
2026-01-09 22:12:42 +0800

    提交

---
+++ src/main/java/com/hao/yu/lc/T086_分隔链表.java
@@ -0,0 +1,18 @@
+package com.hao.yu.lc.listnode;
+
+/**
+ * 说明：
+ *
+ * @author Witty·Kid Fisher
+ * @version v 0.1 2025年12月09日 星期二 17:54
+ */
+public class T086_分隔链表 {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode partition(ListNode head, int x) {
+
+        return head;
+    }
+}
